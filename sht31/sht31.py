a5698b67d88dafc29742da46d4327bb613211332
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2024-08-04 13:30:01 +0800

    refactor: move sht31 constants to own file

--- sht31/sht31.py
+++ sht31/sht31.py
@@ -1,38 +1,15 @@
 import struct
 import time
 
+from sht31.constants import SHT31_ADDRESSES, SHT31_REP_HIGH, \
+    SHT31_SINGLE_COMMANDS, SHT31_CMD_BREAK, \
+    SHT31_CMD_SOFTRESET
 from sht31.exceptions import SHT31ReadError
-
-_SHT31_ADDRESS_DEF = 0x44
-_SHT31_ADDRESS_ALT = 0x45
-_SHT31_ADDRESSES = [_SHT31_ADDRESS_DEF, _SHT31_ADDRESS_ALT]
-
-_SHT31_CMD_SOFTRESET = 0x30A2
-_SHT31_CMD_BREAK = 0x3093
-_SHT31_CMD_HEATER_ON = 0x306D
-_SHT31_CMD_HEATER_OFF = 0x3066
-_SHT31_CMD_READSTATUS = 0xF32D
-_SHT31_CMD_CLEARSTATUS = 0x3041
-
-# group related commands together
-# Attribution: adafruit/Adafruit_CircuitPython_SHT31D
-REP_HIGH = "High"
-REP_MED = "Medium"
-REP_LOW = "Low"
-
-_SHT31_SINGLE_COMMANDS = (
-    (REP_HIGH, True, 0x2C06),
-    (REP_MED, True, 0x2C0D),
-    (REP_LOW, True, 0x2C10),
-    (REP_HIGH, False, 0x2400),
-    (REP_MED, False, 0x240B),
-    (REP_LOW, False, 0x2416)
-)
 
 
 class SHT31:
-    def __init__(self, address=_SHT31_ADDRESS_DEF, bus=None):
-        if address not in _SHT31_ADDRESSES:
+    def __init__(self, address=SHT31_ADDRESSES[0], bus=None):
+        if address not in SHT31_ADDRESSES:
             raise ValueError(f"Invalid I2C address: {hex(address)}!")
 
         if not bus:
@@ -43,7 +20,7 @@ class SHT31:
 
         # use high repeatability single-shot measurement with clock stretching
         # TODO: let user change these settings
-        self._repeatability = REP_HIGH
+        self._repeatability = SHT31_REP_HIGH
         self._clock_stretching = True
 
     def _read_data(self) -> tuple[int, int]:
@@ -80,7 +57,7 @@ class SHT31:
         self._bus.write_i2c_block_data(self._address, cmd[0], [cmd[1]])
 
     def _setup(self) -> None:
-        for command in _SHT31_SINGLE_COMMANDS:
+        for command in SHT31_SINGLE_COMMANDS:
             if (
                 self._repeatability == command[0] and
                 self._clock_stretching == command[1]
@@ -89,9 +66,9 @@ class SHT31:
         time.sleep(0.5)
 
     def reset(self) -> None:
-        self._command(_SHT31_CMD_BREAK)
+        self._command(SHT31_CMD_BREAK)
         time.sleep(0.001)
-        self._command(_SHT31_CMD_SOFTRESET)
+        self._command(SHT31_CMD_SOFTRESET)
         time.sleep(0.0015)
 
     def get_temp_and_humidity(self) -> tuple[float, float]:
