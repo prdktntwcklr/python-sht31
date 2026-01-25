1fa04e4809b6bbdf5c0e4321d99dfda377961da3
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2026-01-25 22:58:08 +0800

    fix: make constants Final

--- sht31/constants.py
+++ sht31/constants.py
@@ -1,21 +1,25 @@
-SHT31_ADDRESS_DEF: int = 0x44  # default address
-SHT31_ADDRESS_ALT: int = 0x45  # alternate address
+"""Defines constants for the sht31 module."""
+
+from typing import Final
+
+SHT31_ADDRESS_DEF: Final[int] = 0x44  # default address
+SHT31_ADDRESS_ALT: Final[int] = 0x45  # alternate address
 SHT31_ADDRESSES: list[int] = [SHT31_ADDRESS_DEF, SHT31_ADDRESS_ALT]
 
-SHT31_CMD_SOFTRESET: int = 0x30A2
-SHT31_CMD_BREAK: int = 0x3093
-SHT31_CMD_HEATER_ON: int = 0x306D
-SHT31_CMD_HEATER_OFF: int = 0x3066
-SHT31_CMD_READSTATUS: int = 0xF32D
-SHT31_CMD_CLEARSTATUS: int = 0x3041
+SHT31_CMD_SOFTRESET: Final[int] = 0x30A2
+SHT31_CMD_BREAK: Final[int] = 0x3093
+SHT31_CMD_HEATER_ON: Final[int] = 0x306D
+SHT31_CMD_HEATER_OFF: Final[int] = 0x3066
+SHT31_CMD_READSTATUS: Final[int] = 0xF32D
+SHT31_CMD_CLEARSTATUS: Final[int] = 0x3041
 
 # group related commands together
 # Attribution: adafruit/Adafruit_CircuitPython_SHT31D
-SHT31_REP_HIGH: str = "High"
-SHT31_REP_MED: str = "Medium"
-SHT31_REP_LOW: str = "Low"
+SHT31_REP_HIGH: Final[str] = "High"
+SHT31_REP_MED: Final[str] = "Medium"
+SHT31_REP_LOW: Final[str] = "Low"
 
-SHT31_SINGLE_COMMANDS: list[tuple[str, bool, int]] = [
+SHT31_SINGLE_COMMANDS: Final[list[tuple[str, bool, int]]] = [
     (SHT31_REP_HIGH, True, 0x2C06),
     (SHT31_REP_MED, True, 0x2C0D),
     (SHT31_REP_LOW, True, 0x2C10),
